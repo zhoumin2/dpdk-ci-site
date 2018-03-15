a8c701e607e421ce0883bddd1e72ebaaaa447b40
Patrick MacArthur <pmacarth@iol.unh.edu>
2018-03-14 20:23:58 -0400

    DPDKLAB-204: Make models inline in admin interface
    It is more natural to edit things in the admin interface if
    components show up underneath their "parent" objects. Use the
    inline form support in the Django admin site interface to make this
    work.
    
    With this change, measurements show up under their respective
    environments, patches show up under their respective patch sets, and
    test results show up under their respective test runs.

--- cisite/results/admin.py
+++ cisite/results/admin.py
@@ -1,14 +1,54 @@
 """Register admin interface for DPDK CI site results models."""
 from django.contrib import admin
-from .models import ContactPolicy, Environment, Measurement, Patch, PatchSet, \
-    Tarball, TestResult, TestRun
+from .models import ContactPolicy, Environment, Measurement, Parameter, \
+    Patch, PatchSet, Tarball, TestResult, TestRun
 from guardian.admin import GuardedModelAdmin
 
-admin.site.register(ContactPolicy)
-admin.site.register(Environment, GuardedModelAdmin)
-admin.site.register(Measurement, GuardedModelAdmin)
-admin.site.register(Patch)
-admin.site.register(PatchSet)
+
+class ContactPolicyInline(admin.StackedInline):
+    """Inline admin form for environment contact policy."""
+
+    can_delete = False
+    model = ContactPolicy
+    verbose_name = 'contact policy'
+    verbose_name_plural = verbose_name
+
+
+class ParameterInline(admin.TabularInline):
+    """Inline admin form for measurement parameters."""
+
+    extra = 0
+    show_change_link = True
+    model = Parameter
+
+
+class MeasurementInline(admin.TabularInline):
+    """Inline admin form for measurements."""
+
+    extra = 0
+    show_change_link = True
+    model = Measurement
+
+
+class PatchInline(admin.StackedInline):
+    """Inline admin form for patches."""
+
+    extra = 0
+    show_change_link = True
+    model = Patch
+
+
+class TestResultInline(admin.TabularInline):
+    """Inline admin form for test results."""
+
+    extra = 0
+    show_change_link = True
+    model = TestResult
+
+
+admin.site.register(Environment, GuardedModelAdmin,
+                    inlines=[ContactPolicyInline, MeasurementInline])
+admin.site.register(Measurement, GuardedModelAdmin, inlines=[ParameterInline])
+admin.site.register(PatchSet, inlines=[PatchInline])
 admin.site.register(Tarball)
-admin.site.register(TestResult, GuardedModelAdmin)
-admin.site.register(TestRun, GuardedModelAdmin)
+admin.site.register(TestRun, GuardedModelAdmin, inlines=[TestResultInline])
