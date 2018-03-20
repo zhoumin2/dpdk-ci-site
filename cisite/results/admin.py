2744a3cc8db67271038e09d28ca65cbe5bd15c5b
Patrick MacArthur <pmacarth@iol.unh.edu>
2018-03-20 12:54:31 -0400

    DPDKLAB-204: Save new contact policy for new environment even if default
    The admin interface was not creating a new contact policy object if the
    user did not modify settings from the default. Fix this so a contact
    policy is always created for each new environment.

--- cisite/results/admin.py
+++ cisite/results/admin.py
@@ -1,8 +1,29 @@
 """Register admin interface for DPDK CI site results models."""
 from django.contrib import admin
+from django.forms.models import ModelForm
 from .models import ContactPolicy, Environment, Measurement, Parameter, \
     Patch, PatchSet, Tarball, TestResult, TestRun
 from guardian.admin import GuardedModelAdmin
+
+
+class AlwaysChangedModelForm(ModelForm):
+    """Display a form which always saves even if not changed.
+
+    This is used for the contact policy inline form, since we always want to
+    save one if we haven't created one yet.
+
+    The code is taken from: https://stackoverflow.com/a/36755438
+    """
+
+    def has_changed(self, *args, **kwargs):
+        """Return true if the form has changed.
+
+        This override always returns true if an object hasn't been created yet,
+        since we always want to create an object the first time.
+        """
+        if self.instance.pk is None:
+            return True
+        return super(AlwaysChangedModelForm, self).has_changed(*args, **kwargs)
 
 
 class ContactPolicyInline(admin.StackedInline):
@@ -12,6 +33,7 @@ class ContactPolicyInline(admin.StackedInline):
     model = ContactPolicy
     verbose_name = 'contact policy'
     verbose_name_plural = verbose_name
+    form = AlwaysChangedModelForm
 
 
 class ParameterInline(admin.TabularInline):
