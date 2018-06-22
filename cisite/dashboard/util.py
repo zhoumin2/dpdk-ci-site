4082b05f3242835ceadb629357efa80fcf64230e
Patrick MacArthur <pmacarth@iol.unh.edu>
2018-06-22 14:22:43 -0400

    DPDKLAB-314: Use user-specified CA certificate bundle if requested
    The Requests library uses its own internal CA database, but we want to
    use the systemwide one which includes the IPA CA certificate. Add a
    Django setting that Ansible can populate to make this possible.

--- cisite/dashboard/util.py
+++ cisite/dashboard/util.py
@@ -1,6 +1,7 @@
 """Define extra utility methods for the dashboard."""
 
 from contextlib import contextmanager
+from django.conf import settings
 from requests import Session
 from requests.adapters import HTTPAdapter
 
@@ -10,6 +11,9 @@ def api_session(request):
     with Session() as s:
         s.mount('http://', HTTPAdapter(max_retries=2))
         s.mount('https://', HTTPAdapter(max_retries=2))
+        s.headers.update({'Referer': request.build_absolute_uri()})
+        if hasattr(settings, 'CA_CERT_BUNDLE'):
+            s.verify = settings.CA_CERT_BUNDLE
         if request.user.is_authenticated and 'api_sessionid' in request.session:
             s.cookies['sessionid'] = request.session['api_sessionid']
         yield s
