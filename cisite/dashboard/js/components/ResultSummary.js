d2a2e7a15f0e86482f27be4e3f7b8b837bb4bb40
Jeremy Plsek <jplsek@iol.unh.edu>
2019-08-21 09:49:32 -0400

    results: fix duplicate entries

--- cisite/dashboard/js/components/ResultSummary.js
+++ cisite/dashboard/js/components/ResultSummary.js
@@ -14,13 +14,20 @@ export default class ResultSummary extends Component {
 
         {Object.values(this.props.obj.result_summary.testcases).map(tc =>
           <div key={tc.testcase.name} title={`${tc.testcase.name}: ${tc.passed} passed, ${tc.failed} failed`}>
-            {[...Array(tc.passed)].map((_, i) =>
-              <span key={i} className="text-success fas fa-check-circle mr-1"></span>
-            )}
+            {/* If a child contains multiple loops, then the 'key' does not
+                work properly, and results in duplicate elements. Thus the
+                loops are wrapped with a span. */}
+            <span>
+              {[...Array(tc.passed)].map((_, i) =>
+                <span key={i} className="text-success fas fa-check-circle mr-1"></span>
+              )}
+            </span>
 
-            {[...Array(tc.failed)].map((_, i) =>
-              <span key={i} className="text-danger fas fa-times-circle mr-1"></span>
-            )}
+            <span>
+              {[...Array(tc.failed)].map((_, i) =>
+                <span key={i} className="text-danger fas fa-times-circle mr-1"></span>
+              )}
+            </span>
           </div>
         )}
       </div>
