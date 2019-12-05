e2ae13a5bd26ebb8e94216b04f4b9679b0aa4f04
Jeremy Plsek <jplsek@iol.unh.edu>
2019-12-05 11:39:14 -0500

    list and detail: add tooltips and fix anchors

--- cisite/dashboard/js/components/ResultSummary.js
+++ cisite/dashboard/js/components/ResultSummary.js
@@ -1,11 +1,15 @@
 import { Component, h } from 'preact'
 
 export default class ResultSummary extends Component {
+  componentDidMount () {
+    $('[data-toggle="tooltip"]').tooltip()
+  }
+
   render () {
     return (
       <div>
         {this.props.obj.result_summary.incomplete > 0 &&
-          <div title={`${this.props.obj.result_summary.incomplete} incomplete environments`} className="text-nowrap d-inline-block mr-4">
+          <div data-toggle="tooltip" title={`${this.props.obj.result_summary.incomplete} incomplete environments`} className="text-nowrap d-inline-block mr-4">
             {[...Array(this.props.obj.result_summary.incomplete)].map((_, i) =>
               <span key={i} className="text-secondary fas fa-circle mr-1"></span>
             )}
@@ -13,7 +17,7 @@ export default class ResultSummary extends Component {
         }
 
         {Object.values(this.props.obj.result_summary.testcases).map(tc =>
-          <div key={tc.testcase.name} title={`${tc.testcase.name}: ${tc.passed} passed, ${tc.failed} failed`} className="text-nowrap d-inline-block mr-4">
+          <div key={tc.testcase.name} data-toggle="tooltip" title={`${tc.testcase.name}: ${tc.passed} passed, ${tc.failed} failed`} className="text-nowrap d-inline-block mr-4">
             {/* If a child contains multiple loops, then the 'key' does not
                 work properly, and results in duplicate elements. Thus the
                 loops are wrapped with a span. */}
